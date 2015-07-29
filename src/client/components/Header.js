06c4ba0c0612f2e132d36b8731cc6bf764a0f68e
thinkingmik <thinkingmik@debian.dev>
2015-07-29 08:40:22 +0200

    Added materil ui component

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import mui from 'material-ui';
+
+let ThemeManager = new mui.Styles.ThemeManager();
+let AppBar = mui.AppBar;
 
 class Header extends React.Component {
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
 
   render() {
   	return (
       <div>
-        Header
+        <AppBar zDepth="0" title="HARA - Realtime React Application" />
       </div>
   	);
   }
 
 }
+
+Header.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
 
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+//Define path
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var appPath = path.resolve(__dirname, 'src', 'client', 'app.js');
-var buildPath = path.resolve(__dirname, 'build', 'js');
+var buildPath = path.resolve(__dirname, 'build');
 var stylesPath = path.resolve(__dirname, 'build', 'assets', 'css', 'styles.css');
+var indexPath = path.resolve(__dirname, '.', 'index.html');
 
 var plugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
   new ExtractTextPlugin(stylesPath, {
     allChunks: true
+  }),
+  new HtmlWebpackPlugin({
+    template: indexPath,
   }),
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NoErrorsPlugin()
@@ -35,7 +42,6 @@ var config = {
   module: {
     loaders: [
       { test: /\.jsx?$/, loaders: ['react-hot', 'babel-loader'], exclude: [nodeModulesPath] },
-      { test: /\.html$/, loader: 'file?name=[name].[ext]' },
       { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass') }
     ]
   }
